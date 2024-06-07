0a0b0a47a1deb7bcf8f3238632d4df4d2658209c
Hassaan Aziz <adaw23haziz@inslaferreria.cat>
2024-06-07 16:11:49 +0200

    Master

---
+++ Prueba/src/prueba/Prueba.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author hassa
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> llista = new ArrayList<>();
+        llista.add("Pa");
+        llista.add("Llet");
+        llista.add("Ous");
+        llista.add("Formatge");
+        llista.add("Tomàquets");
+
+        // Mostrar la llista de la compra
+        System.out.println("Llista de la compra:");
+        for (String article : llista) {
+            System.out.println("- " + article);
+        }
+    }
+    
+}
